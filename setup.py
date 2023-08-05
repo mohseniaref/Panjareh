087c74572a0588426b7f82b0f606e9c2ca01f3e7
Mohammad  Mohseni Aref <mohseni.aref@gmail.com>
2023-08-05 23:38:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-	name = 'slidingwindow',
-	version = '0.0.14',
+	name = 'panjareh',
+	version = '0.0.01',
 	description = 'Sliding Window library for image processing in Python',
-	url = 'https://github.com/adamrehn/slidingwindow',
-	author = 'Adam Rehn',
-	author_email = 'adam@adamrehn.com',
+	url = 'https://github.com/mohseniaref/panjareh',
+	author = 'Mohammad Mohseni Aref',
+	author_email = 'mohseni.aref@gmail.com',
 	license = 'MIT',
-	packages = ['slidingwindow'],
+	packages = ['panjareh'],
 	zip_safe = True,
 	install_requires = [
 		'numpy',
